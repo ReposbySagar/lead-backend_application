23fc1dc016a171f833dcfef764e6c022aa67dc42
ReposbySagar <rsagar1402@gmail.com>
2025-09-25 08:28:10 +0530

    Update LeadQualificationApplication.java

--- src/main/java/com/leadqualification/LeadQualificationApplication.java
+++ src/main/java/com/leadqualification/LeadQualificationApplication.java
@@ -3,7 +3,7 @@ package com.leadqualification;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SystemMetricsAutoConfiguration.class })
 public class LeadQualificationApplication {
 
     public static void main(String[] args) {
